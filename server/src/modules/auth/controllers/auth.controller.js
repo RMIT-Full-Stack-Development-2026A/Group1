af4cc36ef7d05f362bdf3bb81c1c718abced9689
Minh Thang Hoang <176979010+ThangHoang54@users.noreply.github.com>
2026-03-30 00:15:40 +0700

    fix: remove login endpoint from other branch

--- server/src/modules/auth/controllers/auth.controller.js
+++ server/src/modules/auth/controllers/auth.controller.js
@@ -26,45 +26,7 @@ export const AuthController = {
     
     // 
     login: async (req, res) => {
-        try {
-            const { email, username, password } = req.body;
-            const identifier = email || username; // allow to login with either email or username
-
-            if (!identifier || !password) {
-                 return res.status(400).json({
-                    error: "VALIDATION_FAILED",
-                    message: "Missing credentials",
-                    details: [{ field: "identifier/password", cause: "Fields omitted", example: "Provide email/username and password" }]
-                });
-            }
-
-            // run for database and solve for password
-            const user = await AuthService.loginUser(identifier, password);
-
-            // create token and set cookie
-            generateTokenAndSetCookie(res, user._id, user.role); 
-
-            // return Response to Postman/Frontend
-            return res.status(200).json({
-                message: "Login successful",
-                data: {
-                    id: user._id,
-                    username: user.username,
-                    email: user.email,
-                    role: user.role
-                }
-            });
-
-        } catch (error) {
-            if (error.status) {
-                return res.status(error.status).json({
-                    error: error.error,
-                    message: error.message
-                });
-            }
-            console.error("Login Error:", error);
-            return res.status(500).json({ error: "SERVER_ERROR", message: "Internal server error" });
-        }
+        res.status(200).json({ message: "Login endpoint coming soon"})
     },
     logout: async (req, res) => {
         res.status(200).json({ message: "Logout endpoint coming soon!" });
@@ -73,14 +35,7 @@ export const AuthController = {
         res.status(200).json({ message: "CheckAuth endpoint coming soon!" });
     },
     logout: (req, res) => {
-        // Chỉ cần xóa cái Cookie chứa Token đi là xong phim!
-        res.clearCookie("token", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-        });
-
-        return res.status(200).json({
+        res.status(200).json({
             message: "Logout successful"
         });
     },

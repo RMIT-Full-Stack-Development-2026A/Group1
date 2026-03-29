de1acef088aa4d64d65e1012388170b6eba3d045
Kiem Minh <kiemminh000@gmail.com>
2026-03-29 23:25:19 +0700

    feat(auth): add login logic to controller

--- server/src/modules/auth/controllers/auth.controller.js
+++ server/src/modules/auth/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { AuthService } from "../services/auth.service.js";
 import { validateRegisterInput, toUserDTO } from "../dtos/auth.validation.js";
-
+import { generateTokenAndSetCookie } from "../../../utils/token.util.js";
 export const AuthController = {
     register: async (req, res) => {
         try {
@@ -37,7 +37,45 @@ export const AuthController = {
     
     // 
     login: async (req, res) => {
-        res.status(200).json({ message: "Login endpoint coming soon!" });
+        try {
+            const { email, username, password } = req.body;
+            const identifier = email || username; // allow to login with either email or username
+
+            if (!identifier || !password) {
+                 return res.status(400).json({
+                    error: "VALIDATION_FAILED",
+                    message: "Missing credentials",
+                    details: [{ field: "identifier/password", cause: "Fields omitted", example: "Provide email/username and password" }]
+                });
+            }
+
+            // run for database and solve for password
+            const user = await AuthService.loginUser(identifier, password);
+
+            // create token and set cookie
+            generateTokenAndSetCookie(res, user._id, user.role); 
+
+            // return Response to Postman/Frontend
+            return res.status(200).json({
+                message: "Login successful",
+                data: {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    role: user.role
+                }
+            });
+
+        } catch (error) {
+            if (error.status) {
+                return res.status(error.status).json({
+                    error: error.error,
+                    message: error.message
+                });
+            }
+            console.error("Login Error:", error);
+            return res.status(500).json({ error: "SERVER_ERROR", message: "Internal server error" });
+        }
     },
     logout: async (req, res) => {
         res.status(200).json({ message: "Logout endpoint coming soon!" });

bfa63f08129d504e9fa01a59933950bf36f46ba9
Kiem Minh <kiemminh000@gmail.com>
2026-03-30 17:31:02 +0700

    fix(auth): update register response

--- server/src/modules/auth/routes/auth.routes.js
+++ server/src/modules/auth/routes/auth.routes.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 // Public Routes (No token needed)
 router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
+// router.post("/login", AuthController.login);
 
-// Protected Routes (Token Required)
-router.post("/logout", verifyToken, AuthController.logout);
-router.get("/check-auth", verifyToken, AuthController.checkAuth);
+// // Protected Routes (Token Required)
+// router.post("/logout", verifyToken, AuthController.logout);
+// router.get("/check-auth", verifyToken, AuthController.checkAuth);
 
 export default router;

b51db72d15f21da4f519921baa19608bd3c33b10
Minh Thang Hoang <176979010+ThangHoang54@users.noreply.github.com>
2026-04-06 21:21:00 +0700

    refactor(db,index): enhance log detail

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`Connected to MongoDB: ${conn.connection.host}`);
     } catch (error) {
-        console.log("Error connecting to MongoDB", error.message);
+        console.log("Error connecting to MongoDB,", error.message);
         process.exit(1); // 1 is failure, 0 is success
     }
 };

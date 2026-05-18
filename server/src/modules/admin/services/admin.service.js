291d26a269c96ff4338d2243c0faa0fe5d2656bb
Minh Thang Hoang <176979010+ThangHoang54@users.noreply.github.com>
2026-05-18 11:23:28 +0700

    update

--- server/src/modules/admin/services/admin.service.js
+++ server/src/modules/admin/services/admin.service.js
@@ -1,7 +1,6 @@
 import { AuthInterface } from '../../auth/interfaces/auth.interface.js';
 import { GameInterface } from '../../game/interfaces/game.interface.js';
 import { RoomInterface } from '../../room/interfaces/room.interface.js';
-import { SubscriptionInterface } from '../../subscription/interfaces/subscription.interface.js';
 import { AdminDTO } from '../dtos/admin.dto.js';
 import { validatePlayerQuery, validateObjectId, validateAdminRoomQuery } from '../validators/admin.validator.js';
 
